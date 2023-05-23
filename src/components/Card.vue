d18c1a11295a73b332844861fff13fbe1f7e8392
Adarsh Kumar <adarsh010c@gmail.com>
2023-05-24 00:35:54 +0530

    Added the card compoenent

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,14 +1,19 @@
-<script setup></script>
+<script setup>
+const props = defineProps({
+    product: Object
+})
+
+</script>
 
 <template>
     <div class="card">
             <div class="pic-container">
-                <img src="../assets/dress.png" alt="Dress Photo" class="pic">
+                <img :src="props.product.images[1].src" alt="Dress Photo" class="pic">
             </div>
             <div class="card-desc">
                 <div class="info">
-                    <h3>Small Naomi</h3>
-                    <h3>$74.99</h3>
+                    <h3>{{ props.product.title }}</h3>
+                    <h3>${{ props.product.variants[0].price }}</h3>
                 </div>
             </div>
             <button class="btn btn-text">Add to cart</button>
--- src/components/Collection.vue
+++ src/components/Collection.vue
@@ -1,10 +1,16 @@
 <script setup>
 import Card from './Card.vue';
 
+// import Products from json
+import productsJSON from '../assets/products.json'
+
+//take data of 4 products
+const productList = productsJSON.products.slice(4,8);
+
 </script>
 
 <template>
-    <div class="container">
-        <Card/>
+    <div class="container" >
+        <Card v-for="(product,index) in productList" :key="index" :product="product"/>
     </div>
 </template>
